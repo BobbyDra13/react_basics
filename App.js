56e0f156f35e0b71ca65ecb984bcae18082c5dc7
BobbyDra13 <manaschandra07@gmail.com>
2024-04-02 11:16:41 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 const thing = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child1"},[
     React.createElement("h1",{},"This is a h1 tag"), React.createElement("h2",{},"This a h2 tag")
 ]));
